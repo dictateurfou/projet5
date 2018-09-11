9d8a2a3d289956b57894092f6ff706cf20e34e23
dictateurfou <stevenhervy@gmail.com>
2018-09-11 17:28:21 +0200

    Add HTTPRequest class

--- src/Route/Route.php
+++ src/Route/Route.php
@@ -4,6 +4,15 @@ namespace Route;
 
 class Route
 {
+	private $route = [];
+	private $HTTPRequest;
+	 public function __construct(){
+		$this->HTTPRequest = new \Route\HTTPRequest;
+		/*$this->httpResponse = new HTTPResponse($this);*/
+		var_dump($this->HTTPRequest->requestURI());
+	}
 
-
+	public function addRoute($name){
+		array_push($this->route,["name" => $name]);
+	}
 }
