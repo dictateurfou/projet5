bfe69dc4b66629730346e15ec0a4bff08c29f311
dictateurfou <stevenhervy@gmail.com>
2018-09-19 13:24:04 +0200

    viewAll first intégration

--- Entity/Post.php
+++ Entity/Post.php
@@ -12,6 +12,32 @@ class Post
 	private $createdAt;
 	private $editedAt;
 
-	
+	public function getId(){
+		return $this->id;
+	}
+
+	public function getTitle(){
+		return $this->title;
+	}
+
+	public function getImage(){
+		return $this->image;
+	}
+
+	public function getContent(){
+		return $this->content;
+	}
+
+	public function getAuthor(){
+		return $this->author;
+	}
+
+	public function getCreatedAt(){
+		return $this->createdAt;
+	}
+
+	public function getEditedAt(){
+		return $this->editedAt;
+	}
 
 }
