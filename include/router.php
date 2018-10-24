1c9bcd05371a8ca3d29036f7a6f268350f756b11
dictateurfou <stevenhervy@gmail.com>
2018-10-24 15:17:30 +0200

    clean branch

--- include/router.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-
-/*route*/
-$controller->addRoute("account");
-/*fin route*/
-$controller->control();
-include "Controller/".$controller->getController();
-$result = $controller->checkAction();
