dfdc846536115d69ac8c15c4aa41170943654df3
dictateurfou <stevenhervy@gmail.com>
2018-11-26 03:19:40 +0100

    Update PostManager.php

--- Modal/PostManager.php
+++ Modal/PostManager.php
@@ -5,42 +5,4 @@ use PDO;
 
 class PostManager extends Manager{
 
-
-	public function addPost($title,$image,$content){
-		$author = $_SESSION['user']->getId();
-		$cnx = $this->cnx();
-		$date = date('Y-m-d h:m:s');
-		$stmt = $cnx->prepare("INSERT INTO `post`(`title`, `image`, `content`, `author`, `createdAt`, `editedAt`) VALUES (:title,:image,:content,:author,NOW(),NOW())");
-
-		$stmt->bindParam(':title',$title, PDO::PARAM_STR);
-		$stmt->bindParam(':image',$image, PDO::PARAM_STR);
-		$stmt->bindParam(':content',$content, PDO::PARAM_STR);
-		$stmt->bindParam(':author',$author, PDO::PARAM_INT);
-		$stmt->execute();
-	}
-
-	public function edit($title,$content,$id,$image = null){
-		$cnx = $this->cnx();
-		
-		if($image !== null){
-			$stmt = $cnx->prepare("UPDATE post SET `title` = :title,`content` = :content, `image` = :image,`editedAt` = NOW() WHERE `id` = :id");
-			$stmt->bindParam(':image',$image, PDO::PARAM_STR);
-		}
-		else{
-			$stmt = $cnx->prepare("UPDATE post SET `title` = :title,`content` = :content,`editedAt` = NOW() WHERE `id` = :id");
-		}
-
-		$stmt->bindParam(':title',$title, PDO::PARAM_STR);
-		$stmt->bindParam(':content',$content, PDO::PARAM_STR);
-		$stmt->bindParam(':id',$id);
-		$stmt->execute();
-
-	}
-
-	public function delete($id){
-		$cnx = $this->cnx();
-		$stmt = $cnx->prepare("DELETE FROM `post` WHERE id = :id");
-		$stmt->bindParam(':id',$id, PDO::PARAM_INT);
-		$stmt->execute();
-	}
 }
