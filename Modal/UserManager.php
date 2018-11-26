9251dba37de726556740b9b9e6b11fa84263a3b5
dictateurfou <stevenhervy@gmail.com>
2018-11-26 09:19:48 +0100

    Update UserManager.php

--- Modal/UserManager.php
+++ Modal/UserManager.php
@@ -6,39 +6,7 @@ use \Entity\User;
 class UserManager extends Manager{
 
 
-//ajouter edit
-	public function addUser($name,$mail,$pass){
-		$cnx = $this->cnx();
 
-		$stmt = $cnx->prepare("SELECT * FROM user WHERE name = :name OR mail = :mail");
-		$stmt->bindParam(':name', $name);
-		$stmt->bindParam(':mail',$mail);
-		$stmt->execute();
-		$stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Entity\User');
-		$test = $stmt->fetch();
-		
-
-		if($test === false){
-			$stmt = $cnx->prepare("INSERT INTO user (name, pass, mail,avatar,role,validate) VALUES (:name, :pass, :mail,'avatar/default.png',1,'no')");
-			$stmt->bindParam(':name', $name);
-			$stmt->bindParam(':mail', $mail);
-			$stmt->bindParam(':pass', $pass);
-			$stmt->execute();
-
-			$message = "bravo l'inscription c'est bien dérouler vous pouvez a present vous connecter";
-		}
-
-		else{
-			if($test->getName() == $name){
-				$message = "ce nom dutilisateur es déja pris veuillez en choisir un autre";
-			}
-			else{
-				$message = "ce mail es déja enrengistrer";
-			}
-			
-		}
-		return $message;
-	}
 
 	public function getUserById($id){
 		$cnx = $this->cnx();
