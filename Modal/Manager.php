b3bd1de8fa4366a24e5a7394c88715ae547a005e
dictateurfou <stevenhervy@gmail.com>
2018-11-26 03:59:35 +0100

    test

---
+++ Modal/Manager.php
@@ -0,0 +1,28 @@
+<?php
+namespace Modal;
+use PDO;
+abstract class Manager{
+
+	private $dsn;
+	private $user;
+	private $pass;
+
+	public function __construct(){
+		$json = file_get_contents('./config.json');
+		$json_data = json_decode($json,true);
+		$this->dsn = $json_data['bdd']['dsn'];
+		$this->user = $json_data['bdd']['user'];
+		$this->pass = $json_data['bdd']['pass'];
+	}
+
+	protected function cnx(){
+		try {
+			$dbh = new PDO($this->dsn, $this->user, $this->pass);
+		} catch (PDOException $e) {
+		    echo 'Connexion échouée : ' . $e->getMessage();
+		}
+		return $dbh;
+	}
+
+}
+
