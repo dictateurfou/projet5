800beaa45f968612953bb501df775ec5b817b1fd
dictateurfou <stevenhervy@gmail.com>
2018-11-26 03:18:00 +0100

    Update PostManager.php

--- Modal/PostManager.php
+++ Modal/PostManager.php
@@ -5,22 +5,6 @@ use PDO;
 
 class PostManager extends Manager{
 
-	public function viewAll(){
-		$utils = new \Entity\Utils();
-		$post = $utils->getObjectInObject("post",["author"=>'user']);
-		return $post;		
-	}
-
-	public function view($id){
-		$utils = new \Entity\Utils();
-		$post = $utils->getObjectInObject("post",["author"=>'user'],"WHERE post.id = :id",[":id" => $id]);
-		if($post !== false){
-			$post = $post[0];
-		}
-
-		return $post;
-
-	}
 
 	public function addPost($title,$image,$content){
 		$author = $_SESSION['user']->getId();
