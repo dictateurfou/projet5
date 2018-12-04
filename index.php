718833f4dd60608717eb14e2c2a025f71ebdb2c4
dictateurfou <stevenhervy@gmail.com>
2018-12-04 06:57:29 +0100

    change folder Name

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     /*$loader->addNamespace('Blog\Administration', 'src/Blog/Administration/Classes/');*/
     $loader->addNamespace('Entity', 'Entity/');
     $loader->addNamespace('Controller', 'Controller/');
-    $loader->addNamespace('Modal', 'Modal/');
+    $loader->addNamespace('Modal', 'Manager/');
 
     $controller = new \Controller\Controller();
 
